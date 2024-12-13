9d2d08d03141ad918255687bebefeb8a86eca356
MdAnayatullah <mdanayatullah143@gmail.com>
2024-12-13 20:47:03 +0530

    resume link updated

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,13 +3,13 @@ import { SectionWrapper } from "../hoc";
 
 function Resume() {
   const resumeLink =
-    "https://drive.google.com/file/d/1FXPsM92bF7UYGZ1cRjL5VycW29NVzR1H/view?usp=sharing"; // Replace with your actual resume link
+    "https://drive.google.com/file/d/16UQtx-RqmdnJFXIS-6Z938om6VQ-WrMQ/view?usp=sharing"; // Replace with your actual resume link
 
   return (
     <div className="flex justify-start items-start">
       <button
         onClick={() => window.open(resumeLink, "_blank")}
-        className="bg-blue-800 text-white font-bold py-2 px-4 rounded hover:bg-blue-950 rounded-2xl"
+        className="bg-blue-800 text-white font-bold py-2 px-4 hover:bg-blue-950 rounded-2xl"
       >
         View Resume
       </button>
